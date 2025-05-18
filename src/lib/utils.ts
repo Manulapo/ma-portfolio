b9a1ee8d6afe524d909364c3b0a725e4d4f4dfb9
Manulapo <manulaporta98@gmail.com>
2025-05-18 22:29:03 +0200

    fix: simplify getYears function by removing unused end parameter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // returns the years based on the start and put .5 if the actual month is > 6
-export const getYears = (start: number, end = new Date().getFullYear()) => {
+export const getYears = (start: number) => {
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1;
-  const years = end - start;
-  return currentYear === end && currentMonth > 0 ? years + 0.5 : years;
+  const years = currentYear - start;
+  return currentYear && currentMonth > 0 ? years + 0.5 : years;
 };
